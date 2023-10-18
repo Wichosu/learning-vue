b937bab383c5e03b782a8b1e8103fdf8ae1a80db
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-10-17 21:37:18 -0600

    Progress being made, task now showing on list and icons and styles added

--- todo_list/src/App.vue
+++ todo_list/src/App.vue
@@ -3,19 +3,18 @@ import { ref } from 'vue';
 import TaskForm from './components/TaskForm.vue';
 import TaskList from './components/TaskList.vue';
 
-const Tasks = ref([]);
+const Tasks = ref(["Demo Task"]);
 
 const addTask = (task) => {
   Tasks.value.push(task);
   console.log(Tasks.value);
-}
+};
 
 </script>
 
 <template>
   <TaskForm @add-task='addTask' />
-  <TaskList v-bind:todos='Tasks' />
-  
+  <TaskList v-bind:Tasks='Tasks' />
 </template>
 
 <style scoped>
--- todo_list/src/components/TaskList.vue
+++ todo_list/src/components/TaskList.vue
@@ -1,13 +1,23 @@
 <script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(['Tasks']);
 
 </script>
 <template>
   <div class='task-container'>
     <h1>My Tasks</h1>
-    <li>Demo Task</li>
-    <li v-bind:key='task' v-for='task in todos'>
-      {{ task }}
-    </li>
+    <div class='task-list'>
+      <div class='task-item' v-for='task in props.Tasks'>
+        <li>
+          {{ task }}
+        </li>
+        <div class='task-icons'>
+          <span class='task-done'>✔</span>
+          <span class='task-delete'>✘</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,16 +26,70 @@
     border: 1px solid #666;
     border-radius: 4px;
     margin: 2rem auto;
-    width: fit-content;
     max-width: 600px;
     padding: 2rem 4rem;
   }
 
   h1 {
+    text-align: center;
     margin: 0;
   }
 
-  li {
-    margin: 20px 0;
+  .task-list {
+    margin-top: 20px;
+    display: grid;
+    gap: 1rem;
+  }
+
+  .task-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid black;
+    border-radius: 4px;
+    padding: 10px 40px;
+    max-width: 600px;
+    background-color: #f9fafb;
+  }
+
+  .task-item:nth-child(even) {
+    background-color: #dbeafe; 
+  }
+
+  .task-item li {
+    list-style: none;
+  }
+
+  .task-item span {
+    justify-self: flex-end;
+    cursor: pointer;
+  }
+
+  .task-icons {
+    display: flex;
+    gap: 20px;
+  }
+
+  .task-icons span {
+    padding: 5px;
+    border-radius: 8px;
+  }
+
+  .task-done {
+    color: #16a34a;
+  }
+
+  .task-done:hover {
+    color: #f9fafb ;
+    background-color: #22c55e;
+  }
+
+  .task-delete {
+    color: #dc2626;
+  }
+
+  .task-delete:hover {
+    color: #f9fafb;
+    background-color: #dc2626;
   }
 </style>
